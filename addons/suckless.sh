5a93451fb68d4d37e8712c4bf8a88b6e874f92ab
David <foelkdavid@gmail.com>
2021-02-08 18:33:59 +0100

    adding addons

---
+++ addons/suckless.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir /home/.suckless &&
+
+###fetching dependencies
+sudo pacman -S base-devel xorg-server xorg-xinit libx11 libxinerama lixft webkit2gtk &&
+
+###cloning packages
+git clone -C ~/.suckless/ https://git.suckless.org/dwm &&
+git clone -C ~/.suckless/ https://git.suckless.org/st &&
+#git clone -C ~/.suckless/ https://git.suckless.org/slock &&
+
+###creating .xinitrc
+echo "exec dwm" >> ~/.xinitrc &&
+
+###building packages
+cd ~/.suckless/st && sudo make clean install &&
+cd ~/.suckless/dwm && sudo make clean install &&
+
+###editing .bash_profile
+echo "startx" >> ~/.bash_profile
