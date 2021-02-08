fe1e042ad164ba6ae385cb1b0de1cde6919e694c
David <foelkdavid@gmail.com>
2021-02-08 16:46:47 +0100

    adding base files

---
+++ installer1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "initializing archkeys" &&
+pacman-key --init &&
+pacman-key --populate archlinux &&
+
+echo "running pacstrap: loading packages to new system:" &&
+pacstrap /mnt arch-install-scripts base archlinux-keyring linux linux-firmware syslinux sudo iwd nano dhcpcd grub git man binutils make gcc pkg-config fakeroot patch neovim neofetch memtest86+ edk2-shell &&
+echo "done." &&
+
+echo "generating fstab file:" &&
+genfstab -U /mnt >> /mnt/etc/fstab &&
+echo "done." &&
+
+echo "copying arch-installer to new system:" &&
+cp -r /root/arch-installer /mnt/root/ &&
+echo "done!" &&
+
+echo "system is ready for further configuration." &&
+echo "run (arch-chroot /mnt) to proceed."
+
---
+++ installer2.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo "setting timezone:" &&
+ln -sf /usr/share/zoneinfo/Europe/Vienna /etc/localtime &&
+echo "done." &&
+
+echo "syncing system time:" &&
+hwclock --systohc &&
+echo "done." &&
+
+echo "appending locales to locale.gen:" &&
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen &&
+echo "generating locales:" &&
+locale-gen &&
+echo "setting system locale:" &&
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
+echo "done!" &&
+
+echo "setting keymap" &&
+echo "KEYMAP=de-latin1" >> /etc/vconsole.conf &&
+echo "done" &&
+
+echo "setting hostname:" &&
+read -p "Please enter a valid Hostname : " CHN &&
+echo $CHN >> /etc/hostname &&
+echo "127.0.0.1 localhost" >> /etc/hosts &&
+echo "::1" >> /etc/hosts &&
+echo "127.0.1.1 $CHN.localdomain $CHN" >> /etc/hosts &&
+echo "done!"
+
+echo "setting up grub" &&
+read -p "Please enter path for filesystem (eg. /dev/sda) : " FSPI &&
+grub-install --target=i386-pc $FSPI &&
+grub-mkconfig -o /boot/grub/grub.cfg &&
+echo "done!" &&
+
+echo "installing microcode INTEL" &&
+pacman -S intel-ucode &&
+echo "done!" &&
+
+echo "enabling dhcpcd"
+systemctl enable dhcpcd &&
+
+echo "creating new User" &&
+read -p "Please enter a vailid username: " USRNME &&
+useradd -m $USRNME &&
+passwd $USRNME &&
+usermod -a -G wheel $USRNME &&
+
+echo "setting up sudo" &&
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers &&
+echo "%wheel ALL=(ALL) NOPASSWD: /sbin/poweroff, /sbin/reboot, /sbin/shutdown" >> /etc/sudoers &&
+echo "done." &&
+
+echo "locking root user" &&
+passwd -l root &&
+echo "done" &&
+
+echo "everything is installed, you can now reboot the system"
