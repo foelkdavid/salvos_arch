3b0948bfffa93be893dad446dd94756b4dd0e9c3
David Fölk <foelkdavid@gmail.com>
2021-04-18 09:29:22 +0200

    fixing Uefi message

--- UEFI-GRUB.sh
+++ UEFI-GRUB.sh
@@ -4,4 +4,4 @@ pacman -S efibootmgr
 read -p "Please enter path for efi mountpoint: " EFIMP &&
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
-echo "done"
+echo "done. You can Reboot now"
