f9273133aa00d00c858316fa5e46dfbfa793b126
David Fölk <foelkdavid@gmail.com>
2021-04-18 08:31:23 +0200

    Fix UEFI errors

--- UEFI-GRUB.sh
+++ UEFI-GRUB.sh
@@ -2,6 +2,6 @@
 echo "setting up grub for UEFI system:" &&
 pacman -S efibootmgr
 read -p "Please enter path for efi mountpoint: " EFIMP &&
-grub-install --target=i386-pc --efi-directory=$EFIMP --bootloader-id=GRUB &&
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
 echo "done"
